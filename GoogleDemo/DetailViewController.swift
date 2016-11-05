4bf79ff57c040bf64ce556e2c250a5681e9932b1
Adam Noffsinger <adam.noffsinger@uber.com>
2016-11-05 18:06:33 +0100

    Initial commit

---
+++ GoogleDemo/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  GoogleDemo
+//
+//  Created by Adam Noffsinger on 10/11/16.
+//  Copyright © 2016 Adam Noffsinger. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ GoogleDemo/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  GoogleDemo
+//
+//  Created by Adam Noffsinger on 10/11/16.
+//  Copyright © 2016 Adam Noffsinger. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didPressDoneButton(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+
+}
